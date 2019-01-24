7cc8291ded692a9f0919e7c502da037098d62529
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-01-24 15:55:43 -0500

    modulo 04 video 14 agregando un store a platzi video

--- src/entries/home.js
+++ src/entries/home.js
@@ -4,6 +4,22 @@ import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 // console.log('Hola mundo!' )
+import { createStore } from 'redux';
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ 
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
