0c506b15975f5f6f812cc9296ba6849bc1440928
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-01-25 18:43:16 -0500

    modulo 04 video 19 filtrando los datos de busqueda -> tarea

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,13 +1,23 @@
 function data(state,action){
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      const list = state.data.categories[2].playlist;
-      const results = list.filter((item) => {
-                        return item.author.includes(action.payload.query);
-                      })
+      // const list = state.data.categories[2].playlist;
+      var mediaItems = [];      
+      state.data.categories.forEach((cat) => {
+        const results = cat.playlist.filter((item) => {
+                          return item.author.includes(
+                             action.payload.query);
+                        });        
+        if(results.length)
+          mediaItems.push(results)
+            
+      });
+      // const results = list.filter((item) => {
+      //                   return item.author.includes(action.payload.query);
+      //                 })
       return {
         ...state,
-        search: results
+        search: mediaItems
       };
     }
     default:
