354dccff758a10ceef3499e9f6e9e98a533ea455
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-02-05 16:23:15 -0500

    modulo 05 video 31 middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,44 +1,41 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import Home from '../pages/containers/home';
-// import Playlist from './src/playlist/components/playlist';
-// import data from '../api.json';
-// import data from '../schemas/index.js'
-// console.log('Hola mundo!' )
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/index';
 import { Map as map } from 'immutable';
 
-// console.log(data);
-
-// const initialState = {
-//   data: {
-//     // ...data
-//     entities: data.entities,
-//     categories: data.result.categories,
-//     search: [],
-//   },  
-//   modal: {
-//     visibility: false,
-//     mediaId: null,
+// function logger({ getState, dispatch}) {
+//   return (next) => {
+//     return (action) => {
+//       console.log('este es mi viejo estado', getState().toJS());
+//       console.log('vamos a enviar esta acción', action);
+//       const value = next(action);
+//       console.log('este es mi nuevo estado', getState().toJS());
+//       return value
+//     }
 //   }
 // }
+
+const logger = ({ getState, dispatch}) => next => action => {
+  console.log('este es mi viejo estado', getState().toJS());
+  console.log('vamos a enviar esta acción', action);
+  const value = next(action);
+  console.log('este es mi nuevo estado', getState().toJS());
+  return value
+}
 
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ 
-  && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ 
+  // && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
-// console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
-
-// ReactDOM.render(que voy a renderizar, donde lo haré);
-// const holaMundo = <h1>hola Estudiante!</h1>;
 hydrate( 
   <Provider store={store}>
     <Home />
